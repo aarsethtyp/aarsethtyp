e89c464158f784ddca05c49304a78ec320b79f35
aarsethtyp <119767154+aarsethtyp@users.noreply.github.com>
2022-12-03 21:30:38 -0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,42 @@
+export default [
+    {
+        question: "quest?",
+        answers: [
+            { option: "1", correct: true},
+            { option: "2", correct: true},
+            { option: "3", correct: true},
+        ],
+    },
+    {
+        question: "quest?",
+        answers: [
+            { option: "1", correct: false},
+            { option: "2", correct: false},
+            { option: "3", correct: true},
+        ],
+    },
+    {
+        question: "quest?",
+        answers: [
+            { option: "1", correct: false},
+            { option: "2", correct: false},
+            { option: "3", correct: true},
+        ],
+    },
+    {
+        question: "quest?",
+        answers: [
+            { option: "1", correct: true},
+            { option: "2", correct: true},
+            { option: "3", correct: true},
+        ],
+    },
+    {
+        question: "quest?",
+        answers: [
+            { option: "1", correct: true},
+            { option: "2", correct: true},
+            { option: "3", correct: true},
+        ],
+    },
+];
